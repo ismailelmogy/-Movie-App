ce5da1bdfbedfa0e0c25b4a63ad51051e51d78be
Esraa Hassan <esraa.eidhassan@gmail.com>
2019-07-08 09:43:24 -0700

    get request token successfully using Api key

--- Movies App/ViewController.swift
+++ Movies App/ViewController.swift
@@ -7,18 +7,34 @@
 //
 
 import UIKit
+import Foundation
 
 class ViewController: UIViewController {
 
+    
+   
+    
+
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let requestToken = APICallManager.instance.getRequestToken();
+        if (requestToken != "nil"){
+            
+        }
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
 
 
 }
